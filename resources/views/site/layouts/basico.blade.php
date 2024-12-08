83955a7aabed20a4c930b8d8501198839a6a35cb
Zombie <zombiewalkingdead5@gmail.com>
2024-12-08 16:42:22 +0100

    up

--- resources/views/site/layouts/basico.blade.php
+++ resources/views/site/layouts/basico.blade.php
@@ -5,10 +5,14 @@
     <title>SallesForce</title>
     <meta charset="utf-8">
     <link rel="stylesheet" href="{{ asset('css/estilo_site.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
     @yield('conteudo')
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js"></script>
 </body>
 
 </html>
--- resources/views/site/registe.blade.php
+++ resources/views/site/registe.blade.php
@@ -3,20 +3,20 @@
 @section('conteudo')
     <div class="conteudo-pagina">
         <img src="{{ asset('img/logo.jpg') }}" alt="Home" class="menu-icon-top">
-        <h1>SallesForce</h1>
+        <h1 class="titulo">SallesForce</h1>
         <span>Crie uma conta e Comece a Ganhar</span>
         <form action="{{ route('site.cadastro') }}" method="post">
             @csrf
-            <input name="telefone" value="{{ old('telefone') }}" type="text" placeholder="Telefone">
+            <input class="padrao" name="telefone" value="{{ old('telefone') }}" type="text" placeholder="Telefone">
             {{ $errors->has('telefone') ? $errors->first('telefone') : '' }}
             <br>
-            <input name="password" value="{{ old('password') }}" type="password" placeholder="palavra-passe">
+            <input class="padrao" name="password" value="{{ old('password') }}" type="password" placeholder="palavra-passe">
             {{ $errors->has('password') ? $errors->first('password') : '' }}
             <br>
-            <input name="convite" value="{{ old('convite', $convite) }}" type="text" placeholder="código de convite">
+            <input class="padrao" name="convite" value="{{ old('convite', $convite) }}" type="text" placeholder="código de convite">
             {{ $errors->has('convite') ? $errors->first('convite') : '' }}
             <br>
-            <button type="submit">Criar Conta</button>
+            <button class="padrao padbtn" type="submit">Criar Conta</button>
             <br>
             <span>Já tem uma conta? <a href="{{ route('site.login') }}">Iniciar Sessão</a></span>
         </form>
