724513b6ed6d9ba653defdd7c2c4dff1cbd3adad
Zombie <zombiewalkingdead5@gmail.com>
2024-12-11 10:43:30 +0100

    up maybe conflict

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -129,6 +129,12 @@ class UserController extends Controller
         $user->incomeTotal += $valorDepositado;
         $user->save();
 
+        Record::create([
+            'name' => 'Bônus Especial',
+            'money' => $valorDepositado,
+            'user_id' => $user->id,
+        ]);
+
         // Retornar com uma mensagem de sucesso
         return redirect()->back()->with('success', 'Depósito realizado com sucesso!');
     }
@@ -169,15 +175,6 @@ class UserController extends Controller
         $remainingTotal = $machine->duration; // Duração total da máquina
         $incomeTotal = 0; // Rendimento inicial
 
-        // Verificar se o usuário já coletou hoje
-        if ($user->last_collection === $today) {
-            $remainingTotal -= 1; // Reduzir 1 dia se a máquina já foi usada hoje
-            $incomeTotal += $machine->income;
-            $user->incomeToday += $machine->income;
-            $user->incomeTotal += $machine->income;
-            $user->save();
-        }
-
         // Anexar a máquina ao usuário na tabela pivot
         $user->machines()->attach($machine->id, [
             'remainingToday' => 24 * 60 * 60, // Tempo restante em segundos
@@ -193,54 +190,59 @@ class UserController extends Controller
         $user->incomeDaily += $machine->income;
         $user->save(); // O save deve funcionar agora se o User for um modelo Eloquent
 
-        // Comissão de 10% para o superior de nível 1 (direto)
-        $superiorNivel1 = User::find($user->userId);
-        if ($superiorNivel1) {
+        if (!$user->isVip) {
+
+            // Comissão de 10% para o superior de nível 1 (direto)
+            $superiorNivel1 = User::find($user->userId);
+            if ($superiorNivel1) {
             $comissaoNivel1 = $machine->price * 0.10;
-            $superiorNivel1->money += $comissaoNivel1;
-            $superiorNivel1->incomeToday += $comissaoNivel1;
-            $superiorNivel1->incomeTotal += $comissaoNivel1;
-            $superiorNivel1->save();
+                $superiorNivel1->money += $comissaoNivel1;
+                $superiorNivel1->incomeToday += $comissaoNivel1;
+                $superiorNivel1->incomeTotal += $comissaoNivel1;
+                $superiorNivel1->save();
 
-            Record::create([
-                'name' => 'Bônus Convidado',
-                'money' => $comissaoNivel1,
-                'user_id' => $superiorNivel1->id,
-            ]);
+                Record::create([
+                    'name' => 'Bônus Convidado',
+                    'money' => $comissaoNivel1,
+                    'user_id' => $superiorNivel1->id,
+                ]);
+            }
+
+            // Comissão de 5% para o superior de nível 2 (indiretamente)
+            $superiorNivel2 = User::find($superiorNivel1 ? $superiorNivel1->userId : null);
+            if ($superiorNivel2) {
+                $comissaoNivel2 = $machine->price * 0.05;
+                $superiorNivel2->money += $comissaoNivel2;
+                $superiorNivel2->incomeToday += $comissaoNivel2;
+                $superiorNivel2->incomeTotal += $comissaoNivel2;
+                $superiorNivel2->save();
+
+                Record::create([
+                    'name' => 'Bônus Convidado',
+                    'money' => $comissaoNivel2,
+                    'user_id' => $superiorNivel2->id,
+                ]);
+            }
+
+            // Comissão de 2% para o superior de nível 3 (indiretamente)
+            $superiorNivel3 = User::find($superiorNivel2 ? $superiorNivel2->userId : null);
+            if ($superiorNivel3) {
+                $comissaoNivel3 = $machine->price * 0.02;
+                $superiorNivel3->money += $comissaoNivel3;
+                $superiorNivel3->incomeToday += $comissaoNivel3;
+                $superiorNivel3->incomeTotal += $comissaoNivel3;
+                $superiorNivel3->save();
+
+                Record::create([
+                    'name' => 'Bônus Convidado',
+                    'money' => $comissaoNivel3,
+                    'user_id' => $superiorNivel3->id,
+                ]);
+            }
+
+            $user->isVip = true;
+            $user->save();
         }
-
-        // Comissão de 5% para o superior de nível 2 (indiretamente)
-        $superiorNivel2 = User::find($superiorNivel1 ? $superiorNivel1->userId : null);
-        if ($superiorNivel2) {
-            $comissaoNivel2 = $machine->price * 0.05;
-            $superiorNivel2->money += $comissaoNivel2;
-            $superiorNivel2->incomeToday += $comissaoNivel2;
-            $superiorNivel2->incomeTotal += $comissaoNivel2;
-            $superiorNivel2->save();
-
-            Record::create([
-                'name' => 'Bônus Convidado',
-                'money' => $comissaoNivel2,
-                'user_id' => $superiorNivel2->id,
-            ]);
-        }
-
-        // Comissão de 2% para o superior de nível 3 (indiretamente)
-        $superiorNivel3 = User::find($superiorNivel2 ? $superiorNivel2->userId : null);
-        if ($superiorNivel3) {
-            $comissaoNivel3 = $machine->price * 0.02;
-            $superiorNivel3->money += $comissaoNivel3;
-            $superiorNivel3->incomeToday += $comissaoNivel3;
-            $superiorNivel3->incomeTotal += $comissaoNivel3;
-            $superiorNivel3->save();
-
-            Record::create([
-                'name' => 'Bônus Convidado',
-                'money' => $comissaoNivel3,
-                'user_id' => $superiorNivel3->id,
-            ]);
-        }
-
 
         return redirect()->back()->with('success', 'Máquina alugada com sucesso!');
     }
@@ -268,88 +270,36 @@ class UserController extends Controller
         $userId = Auth::user();
         $user = User::find($userId->id);
 
-        // Verificar se o usuário já coletou hoje
-        $today = now()->toDateString();
-        if ($user->last_collection === $today) {
-            return redirect()->back()->with('error', 'Você já coletou as recompensas de hoje.');
-        }
-
         // Recuperar todas as máquinas do usuário com os dados da pivot
-        $machines = $user->machines()->withPivot('incomeTotal', 'remainingTotal')->get();
+        $machines = $user->machines()->withPivot('incomeTotal', 'last_collection')->get();
 
         // Iterar pelas máquinas para calcular o total, atualizar incomeTotal e decrementar remainingTotal
         foreach ($machines as $machine) {
+
+            // Verificar se a máquina já coletou hoje
+            $today = now()->toDateString();
+            if ($machine->pivot->last_collection === $today) continue;
+
             $income = $machine->income;
 
-            // Decrementar o campo remainingTotal na pivot
-            $newRemainingTotal = $machine->pivot->remainingTotal - 1;
+            // Atualizar o saldo do usuário
+            $user->money += $income;
+            $user->incomeToday += $income;
+            $user->incomeTotal += $income;
+            $user->save();
 
-            if ($newRemainingTotal > 0) {
-                // Atualizar incomeTotal e remainingTotal se ainda houver tempo restante
-                $user->machines()->updateExistingPivot($machine->id, [
-                    'incomeTotal' => $machine->pivot->incomeTotal + $income,
-                    'remainingTotal' => $newRemainingTotal,
-                ]);
+            $user->machines()->updateExistingPivot($machine->id, [
+                'incomeTotal' => $machine->pivot->incomeTotal + $income,
+                'last_collection' => $today,
+            ]);
 
-                Record::create([
-                    'name' => 'Renda Diária',
-                    'money' => $income,
-                    'user_id' => $user->id,
-                ]);
-
-            } else {
-                // Remover a relação se remainingTotal chegar a 0
-                $user->machines()->detach($machine->id);
-                $user->incomeDaily -= $machine->income;
-                $user->save();
-            }
+            Record::create([
+                'name' => 'Renda Diária',
+                'money' => $income,
+                'user_id' => $user->id,
+            ]);
         }
 
-        // Atualizar o saldo do usuário
-        $user->money += $user->incomeDaily;
-        $user->incomeToday = $user->incomeDaily;
-        $user->incomeTotal += $user->incomeDaily;
-        $user->last_collection = $today;
-        $user->save();
-
-        return redirect()->back()->with('success', "Recompensas coletadas com sucesso! Total: {$user->incomeDaily} kz.");
-    }
-
-
-
-
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\User  $user
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(User $user)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\User  $user
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, User $user)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\User  $user
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(User $user)
-    {
-        //
+        return redirect()->back()->with('success', 'Recompensas coletadas com sucesso');
     }
 }
